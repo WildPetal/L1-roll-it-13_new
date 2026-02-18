242572d2261ce811728c22397e05a2751759010b
WildPetal <sullivanp60424@masseyhigh.school.nz>
2026-02-18 19:32:59 +1300

    Add files via upload
    instructions

---
+++ C_02_instructiond.py
@@ -0,0 +1,28 @@
+
+#functions go there
+
+def yes_no(question):
+
+    """checks user response to a question is yes / no (y/n), returns 'yes' or 'no'"""
+
+    while True:
+
+        response = input(question).lower()
+
+        #check the user says yes / no
+        if  response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("please say yes / no")
+
+
+# Main routine
+
+# testing loop
+while True:
+    want_instructions = yes_no("Do you want to see the instructions")
+    print(f"you chose {want_instructions}")
+
+print("we done")
