de6808901f48a73a236bd6c36a3b75cc02738b44
WildPetal <sullivanp60424@masseyhigh.school.nz>
2026-02-27 09:50:14 +1300

    Add files via upload
    broken

--- C_04_one_round_v2.py
+++ C_04_one_round_v2.py
@@ -18,26 +18,95 @@ def initial_points(which_player):
 
     return total, double
 
-# Intialise rounds points
-user_points = 0
-comp_points = 0
+# main starts here...
 
+# Roll the dice for the user and note if they got a double
+initial_user = initial_points("user")
+initial_comp = initial_points("comp")
 
-# roll the dice for the computer
-comp_one = random.randint(1, 6)
-comp_two = random.randint(1, 6)
+# Retrieve user points (first item returned from function)
+user_points = initial_user[0]
+comp_points = initial_user[0]
 
-
-# Update the dice for the computer points
-user_points = user_one + user_two
-comp_points = comp_two + user_one
-
-# Output the user / computer points
-# Let the user  know   if they qualify for double points
-print("\ninitial points")
-print(f"User        - Roll 1: {user_one} \t| Roll 2: {user_two} \t| Points: {user_points}")
-print(f"computer    - Roll 1: {comp_one} \t| Roll 2: {comp_two} \t| Points: {comp_points}")
+double_user =initial_user[1]
 
 # Let the user know if they qualify for double points
 if double_user == "yes":
     print("Great news - if you win, you will earn double points!")
+
+# assume user goes first...
+first = "user"
+second = "computer"
+player_1_points = user_points
+player_2_points = comp_points
+
+# if user has fewer points, they start the game
+if user_points < comp_points:
+    print("You start because your initial roll was less than the computer\n")
+
+# if the user and computer roll equal points, the users is player 1...
+elif user_points == comp_points:
+    print("The initial rolls were the same, the user starts!")
+
+# if the computer has fewer points, switch the computer to 'player 1'
+else:
+    player_1_points, player_2_points = player_2_points, player_1_points
+    first, second = second, first
+
+# Loop until we have a winner...
+while player_1_points < 13 and player_2_points < 13:
+    print()
+    input("press <enter> to continue this round\n")
+
+    # first person rolls the die and score is updated
+    player_1_roll = random.randint(1, 6)
+    player_1_points += player_1_roll
+
+    print(f"{first}: Rolled a {player_1_roll} - has {player_1_points} points")
+
+    # if the first person's score is over 13, end the round
+    if player_1_points >= 13:
+        break
+
+    #second person rolls the die (and score is updated)
+    player_2_roll = random.randint(1, 6)
+    player_2_points += player_2_roll
+
+    print(f"{second}: Rolled a {player_2_roll} - has {player_2_points} points")
+
+    print(f"{first}: {player_1_points} | {second}: {player_2_points}")
+
+    # end of round
+
+    #associate player points with either the user or the computer
+    user_points = initial_points
+    comp_points = initial_points
+
+    # switch the user and computer points if the computer went first
+    if first == "computer":
+        user_points, comp_points = comp_points, user_points
+
+    # work out who won
+    if user_points > comp_points:
+        winner = "user"
+    else:
+        winner = "computer"
+
+    round_feedback = f"the {winner} won."
+
+    # double user points if eligible
+    if winner == "user" and double_user == "yes":
+        user_points = user_points * 2
+
+    # Output round results
+    print("\nRound results")
+    print(f"User Points: {user_points} | Computer Points: {comp_points}")
+    print(round_feedback)
+    print()
+
+
+
+
+
+
+
